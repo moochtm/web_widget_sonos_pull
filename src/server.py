455c75db5cab85ea674494672a1effbaa706110e
home <moochtm@hotmail.com>
2022-01-18 10:14:32 +0000

    Move to using Jinja2 for Sonos widget HTML

--- src/server.py
+++ src/server.py
@@ -11,7 +11,7 @@ import urllib.parse
 import uuid
 
 import aiofiles
-import aiohttp  # need version 4.0.0.a to handle disconnects properly!
+import aiohttp
 import aiohttp_jinja2
 import jinja2
 import soco
@@ -20,7 +20,9 @@ from soco.music_services import MusicService
 
 # SET UP LOGGING
 logging.basicConfig(
-    format="%(asctime)s | %(levelname)-7s | %(name)-25s: %(message)s", level=logging.INFO, datefmt="%H:%M:%S"
+    format="%(asctime)s | %(levelname)-7s | %(name)-25s: %(message)s",
+    level=logging.INFO,
+    datefmt="%H:%M:%S",
 )
 logger = logging.getLogger(__name__)
 
@@ -32,7 +34,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 HOST_IP_ADDRESS = s.getsockname()[0]
 s.close()
-logger.info(f'HOST_IP_ADDRESS: {HOST_IP_ADDRESS}')
+logger.info(f"HOST_IP_ADDRESS: {HOST_IP_ADDRESS}")
 
 
 class Server:
@@ -58,9 +60,10 @@ class Server:
             return
         else:
             ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-            ssl_context.load_cert_chain('domain_srv.crt', 'domain_srv.key')
-            web.run_app(self.app, host=self.host, port=self.port, ssl_context=ssl_context)
-
+            ssl_context.load_cert_chain("domain_srv.crt", "domain_srv.key")
+            web.run_app(
+                self.app, host=self.host, port=self.port, ssl_context=ssl_context
+            )
 
     async def _init_app(self):
         """
@@ -71,7 +74,7 @@ class Server:
         app.on_shutdown.append(self._shutdown_app)
         aiohttp_jinja2.setup(app, loader=jinja2.PackageLoader("src", "templates"))
         app.router.add_get("/", self.index)
-        app.router.add_get("/images", self.images)
+        app.router.add_get("/image_proxy", self.image_proxy)
         app.router.add_static("/static/", path=PROJECT_ROOT / "static", name="static")
         return app
 
@@ -85,9 +88,9 @@ class Server:
 
     def _get_protocol(self):
         if self.http:
-            return 'http'
+            return "http"
         else:
-            return 'https'
+            return "https"
 
     def _get_page(self, request):
         """
@@ -120,39 +123,39 @@ class Server:
         self._remove_ws(request, ws_identifier)
         return ws_current
 
-    async def images(self, request):
+    async def image_proxy(self, request):
         """
-        Handles /images requests.
-        Requests always come in in form /images?url=image_url
-        First, there's some clean-up done on the images folder, then
+        Handles /image_proxy requests.
+        Requests always come in in form /image_proxy?url=image_url
+        First, there's some clean-up done on the image_proxy folder, then
         Image_url is downloaded if hasn't been already, and then returned
-        This ensures all images are served from the same domain (better browser compatibility)
+        This ensures all image_proxy are served from the same domain (better browser compatibility)
         """
         params = request.rel_url.query
-        # get images folder path
-        dp = os.path.join('.', 'images')
+        # get image_proxy folder path
+        dp = os.path.join(".", "image_proxy")
         if not os.path.exists(dp):
             os.mkdir(dp)
 
-        # Do some clean-up of the images folder
+        # Do some clean-up of the image_proxy folder
         for f in os.listdir(dp):
             fp = os.path.join(dp, f)
             file_last_accessed = int(os.path.getatime(fp))
             now = int(time.time())
             if now - file_last_accessed > 8 * 60 * 60:
-                logger.info(f'Images clean-up removing file: {fp}')
+                logger.info(f"Images clean-up removing file: {fp}")
                 os.remove(fp)
 
         # Now focus on the requested file
         async with aiohttp.ClientSession() as session:
-            url = params['url']
+            url = params["url"]
             url_hash = hashlib.sha1(url.encode("UTF-8")).hexdigest()
-            fp = os.path.join(dp, url_hash + '.jpeg')
+            fp = os.path.join(dp, url_hash + ".jpeg")
             if not os.path.exists(fp):
                 async with session.get(url) as resp:
                     if resp.status == 200:
-                        fp = os.path.join('.', 'images', url_hash + '.jpeg')
-                        async with aiofiles.open(fp, mode='wb+') as f:
+                        fp = os.path.join(dp, url_hash + ".jpeg")
+                        async with aiofiles.open(fp, mode="wb+") as f:
                             await f.write(await resp.read())
                             await f.close()
             resp = web.FileResponse(fp)
@@ -173,7 +176,7 @@ class Server:
         if msg.type != aiohttp.WSMsgType.TEXT:
             return
         data_dict = json.loads(msg.data)
-        logger.info(f'Msg from client {ws_identifier}: {data_dict}')
+        logger.info(f"Msg from client {ws_identifier}: {data_dict}")
         if "action" not in data_dict:
             return
         action = data_dict["action"]
@@ -187,7 +190,7 @@ class Server:
         Sends Sonos info to web socket.
         """
         try:
-            logger.info(f'Responding to client {ws_identifier}')
+            logger.info(f"Responding to client {ws_identifier}")
             try:
                 # get Sonos device and general info
                 device = soco.discovery.by_name(sonos_name)
@@ -209,35 +212,33 @@ class Server:
             # if there's no img_src in track info, we might be playing a radio station
             else:
                 channel = media_info["channel"]
-                tunein = MusicService('TuneIn')
+                tunein = MusicService("TuneIn")
                 # search Tunein for value of channel
-                results = tunein.search(category='stations', term=channel)
+                results = tunein.search(category="stations", term=channel)
                 if len(results) > 0:
                     result = results[0]
-                    img_src = result.metadata['stream_metadata'].metadata['logo']
+                    img_src = result.metadata["stream_metadata"].metadata["logo"]
                 title = channel
                 artist = ""
                 album = ""
             # encode artwork URL ready for use as parameter
             img_src = urllib.parse.quote(img_src)
             # point artwork URL back to our server for serving/downloading
-            img_src = f"{self.protocol}://{HOST_IP_ADDRESS}:{self.port}/images?url={img_src}"
-            # If status is PLAYING, display artwork, title, etc...
-            if transport == "PLAYING":
-                html = f"""
-                <div><img class="artwork" src="{img_src}"></img></div>
-                <div class="title">{title}</div>
-                <div class="artist">{artist} - {album}</div>
-                """
-            # ...else say nothing is playing.
-            else:
-                html = '<div class="artist">Nothing playing right now...</div>'
-            logger.info(f'Sending to client {ws_identifier}: {html}')
+            img_src = f"{self.protocol}://{HOST_IP_ADDRESS}:{self.port}/image_proxy?url={img_src}"
+            # Make context dict
+            context = {
+                "transport": transport,
+                "title": title,
+                "artist": artist,
+                "album": album,
+                "img_src": img_src,
+            }
+            html = aiohttp_jinja2.render_string("widget_sonos.html", request, context)
+            print(html)
+            logger.debug(f"Sending to client {ws_identifier}: {html}")
             # send HTML
             ws = request.app["websockets"][ws_identifier]
-            await ws.send_json({
-                "html": html
-            })
+            await ws.send_json({"html": html})
         except ConnectionResetError:
             self._remove_ws(request, ws_identifier)
             return
